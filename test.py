f84e86bfd7d1497eb1aedefd4f0df6f39d1fc5b4
SpringDev540 <springfx.intro@outlook.jp>
2025-06-03 08:16:02 +0000

    テスト1

--- test.py
+++ test.py
@@ -1,18 +1,2 @@
 # word_test.py
-
-
-
-print("=== 英単語テスト ===")
-print("日本語の意味を入力してください。\n")
-
-# 単語ごとにテスト
-for english, japanese in words.items():
-    answer = input(f"{english} の意味は？：")
-    if answer == japanese:
-        print("⭕ 正解！\n")
-        score += 1
-    else:
-        print(f"❌ 不正解。正しくは「{japanese}」です。\n")
-
-# 結果表示
-print(f"あなたのスコア：{score} / {len(words)} 点")
+aaaaaaaaaaaaaaaaaaa
